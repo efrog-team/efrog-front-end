a12e51e7bcc8b51fa940a6b8c57209fb081ec882
tsaplia <yunnakheifets@gmail.com>
2023-11-30 12:41:52 +0100

    Added loc memorisation

--- main/src/routes/+page.server.ts
+++ main/src/routes/+page.server.ts
@@ -1,5 +1,7 @@
+import { localisations } from "$lib/config.js";
 import { redirect } from "@sveltejs/kit";
 
-export function load() {
-	throw redirect(303, "/uk");
+export function load({cookies}) {
+	let lang = localisations.includes(cookies.get('lang')||"") ? cookies.get('lang') as string : 'uk';
+	throw redirect(303, `/${lang}`);
 }
