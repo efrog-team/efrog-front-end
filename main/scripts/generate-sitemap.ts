9858d48647186a8e8eddaf24820d8a0663185ec6
tsaplia <yunnakheifets@gmail.com>
2024-02-16 22:05:57 +0100

    Chaged sitemap location

--- main/scripts/generate-sitemap.ts
+++ main/scripts/generate-sitemap.ts
@@ -2,14 +2,21 @@ import dirTree from 'directory-tree';
 import * as fs from "fs";
 import { localisations } from '../src/lib/config';
 
+
 let baseRoute = "/";
-let routes: string[] = [baseRoute]
+let excludedPaths = [
+    /^\/editor\/.*$/,
+    /^\/contests\/participated$/,
+    /\/question\/submitted/
+];
+
+let routes: string[] = []
 let date = new Date().toISOString().split('T')[0];
 let domain = "https://efrog.pp.ua"
 
 function getSitemapXML(routes: string[]) {
     let sitemap = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-    sitemap += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n"
+    sitemap += "<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xhtml=\"http://www.w3.org/1999/xhtml\" >\n"
     routes.forEach(route => {
         localisations.forEach((loc)=>{
             sitemap += getSitemapUrl(loc, route);
@@ -23,7 +30,7 @@ function getSitemapXML(routes: string[]) {
 function getSitemapUrl(lang:string, route: string) {
     let url = `<url>\n<loc>${domain}/${lang}${route}</loc>\n`;
     localisations.forEach((loc)=>{
-        url += `<link rel="alternate" hreflang="${loc}" href="${domain}/${loc}${route}">\n`
+        url += `<xhtml:link rel="alternate" hreflang="${loc}" href="${domain}/${loc}${route}" />\n`
     });
     url += `<lastmod>${date}</lastmod>\n</url>\n`;
     return url
@@ -36,7 +43,8 @@ function getEndpoints(tree: dirTree.DirectoryTree, route: string) {
 
             let childRoute = route + child.name;
             if (child.children.some(e => e.name === '+page.svelte')) {
-                routes.push(childRoute.replace(/\/\(.*\)/, ""));
+                let pageRoute = childRoute.replace(/\/\(.*\)/, "");
+                if(!excludedPaths.some(e => e.test(pageRoute))) routes.push(pageRoute);
             }
             getEndpoints(child, childRoute + "/");
         }
@@ -56,4 +64,4 @@ const sitemap = getSitemapXML(routes)
 // fs.writeFileSync('.svelte-kit/output/client/sitemap.xml', sitemap);
 
 // If you use the script in prebuild mode use
-fs.writeFileSync('static/sitemap.xml', sitemap);
+fs.writeFileSync('static/sitemaps/sitemap.xml', sitemap);
